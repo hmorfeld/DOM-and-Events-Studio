614e5155c08e94130fa54586537949da191bb184
Heather Morfeld <hmorfeld91@gmail.com>
2021-03-11 22:03:55 -0600

    completed part 1

--- scripts.js
+++ scripts.js
@@ -1,2 +1,25 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+
+
+window.addEventListener("load", function{
+    let rocketImg = document.getElementById('rocket');
+    rocketImg.style.position = "absolute";
+    rocketImg.style.left = "0px";
+    rocketImg.style.bottom = "0px";
+    let status = document.getElementById('flightStatus');
+    let shuttleHeight = document.getElementById('spaceShuttleHeight');
+    let backgroundColor = document.getElementById('shuttleBackground');
+    let takeOff = document.getElementById('takeoff');
+    let landing = document.getElementById('landing');
+    let missionAbort = document.getElementById('missionAbort')
+
+    takeOff.addEventListener("click", function{
+        let result = window.confirm("Are you sure your ready for Take Off?");
+        if (result){
+            status.innerText = "Shuttle is in Flight";
+            backgroundColor.style.backgroundColor = "Blue";
+            shuttleHeight.innerText = '10000';
+        }
+    })
+    
+
+    
