56447e755f8287cdc99c536545a7fa7afa1050f5
Heather Morfeld <hmorfeld91@gmail.com>
2021-03-11 22:38:46 -0600

    added landing and mission abort

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 
-window.addEventListener("load", function{
+window.addEventListener("load", function(){
     let rocketImg = document.getElementById('rocket');
     rocketImg.style.position = "absolute";
     rocketImg.style.left = "0px";
@@ -11,15 +11,39 @@ window.addEventListener("load", function{
     let takeOff = document.getElementById('takeoff');
     let landing = document.getElementById('landing');
     let missionAbort = document.getElementById('missionAbort')
+    let astroChat = document.getElementById('astroChat')
+    let up = document.getElementById('up');
+    let up = document.getElementById('down');
+    let up = document.getElementById('left');
+    let up = document.getElementById('right');
 
-    takeOff.addEventListener("click", function{
-        let result = window.confirm("Are you sure your ready for Take Off?");
+    takeOff.addEventListener("click", function(){
+        let result = window.confirm("WARNING: ARE YOU READY FOR TAKE OFF?");
         if (result){
             status.innerText = "Shuttle is in Flight";
+            astroChat.innerText = "Have a good trip!"
             backgroundColor.style.backgroundColor = "Blue";
             shuttleHeight.innerText = '10000';
+         }
+    })
+
+    landing.addEventListener("click", function(){
+        let result = window.confirm("ROCKET IS LANDING! Engage Landing Gear!");
+        if (result){
+            status.innerText = "Shuttle has landed";
+            astroChat.innerText = "Welcome Back!"
+            backgroundColor.style.backgroundColor = "Green";
+            shuttleHeight.innerText = '0';
         }
     })
-    
 
-    
+    missionAbort.addEventListener("click", function(){
+        let result = window.confirm("WARNING: Are your sure you want to abort this mission?");
+        if (result){
+            status.innerText = "Mission aborted";
+            astroChat.innerText = "Mission was aborted. Please refresh page to start again."
+            backgroundColor.style.backgroundColor = "Red";
+            shuttleHeight.innerText = '0';
+        }
+    })
+})
